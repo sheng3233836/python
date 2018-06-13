c7f29e01782f61c52a0b0322d651d33b4eee13d8
xinyx.yuan <xinyx.yuan@qunar.com>
2018-06-13 12:02:21 +0800

    add comment

--- game/game_utils.py
+++ game/game_utils.py
@@ -3,10 +3,22 @@ import autopy
 
 
 def resize_image(image_path, w_scale=1.0, h_scale=1.0):
+    """
+    Scale up the image
+    :param image_path: image path
+    :param w_scale: width scale
+    :param h_scale: height scale
+    :return: image
+    """
     image = Image.open(image_path)
     width, height = image.size
     return image.resize((round(width * w_scale), round(height * h_scale)), Image.ANTIALIAS)
 
 
 def format_color(rgb_color):
+    """
+    format rgb to hex
+    :param rgb_color:
+    :return:
+    """
     return hex(autopy.color.rgb_to_hex(rgb_color[0], rgb_color[1], rgb_color[2]))
